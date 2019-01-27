b9cb92bb77e328807f980a2561f710480b603219
luxury <erikbatista42@gmail.com>
2019-01-26 18:50:12 -0800

    opens files and reads each words and converts it into a string

--- dictonary_words.py
+++ dictonary_words.py
@@ -1,8 +1,12 @@
+import random
+
+
 '''read in the words file'''
-
-
+file = open("20k.txt", "r")
 
 '''select a random set of words from the file and store in a data type'''
+words = file.read().split()
+print(words)
 
 
 
