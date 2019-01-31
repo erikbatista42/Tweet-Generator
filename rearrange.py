28b77aaa2cf99b9836e80b67c47a0a42bffdc794
luxury <erikbatista42@gmail.com>
2019-01-30 17:17:13 -0800

    updated rearrange.py

--- rearrange.py
+++ rearrange.py
@@ -13,9 +13,9 @@ def rearrangeArguments():
 
     while len(arguments) > 0:
         # passes all items in a random order to rearrangedArgument's list
-        randomItem = random.randint(0, len(arguments) -1)
-        rearrangedArguments.append(arguments[randomItem])
-        arguments.pop(randomItem)
+        index = random.randint(0, len(arguments) -1)
+        rearrangedArguments.append(arguments[index])
+        arguments.pop(index)
 
     return " ".join(rearrangedArguments)
 
