17cf279650d1bbef3e5453858d19783a0490509a
luxury <erikbatista42@gmail.com>
2019-01-31 10:29:00 -0800

    added function templates for histograms.py

---
+++ histograms.py
@@ -0,0 +1,16 @@
+def histogram(txtFile):
+    #return a histogram data structure that stores each unique word along with the number of times the word appears in the source text.
+    pass
+
+
+def unique_words(histogram):
+    #returns the total count of unique words in the histogram
+    pass
+
+def frequency(histogram, word):
+    #returns the number of times that word appears in a text.
+    #For example, when given the word "mystery" and the Holmes histogram, it will return the integer 20.
+    pass
+
+if __name__ == "__main__":
+    pass
