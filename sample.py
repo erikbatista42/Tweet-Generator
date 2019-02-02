f44710b8beeac61c14eaacfedeaef7ffa771f931
luxury <erikbatista42@gmail.com>
2019-02-02 01:10:29 -0800

    update

--- sample.py
+++ sample.py
@@ -1,9 +1,6 @@
 import sys
 import random
 
-'''Run this file in the command line'''
-
-
 def sample():
     #select a random word out of the text file that is typed on the terminal
     file_name = sys.argv[1]
@@ -14,6 +11,30 @@ def sample():
 
     return index
 
+def random_index():
+    '''
+    Make the .choice method but words will be chosen based on frequency.
+    So if the word 'fish' appears 3x and 'erik' appears 1x,
+    the program will choose the most *frequent* word. "fish" like 75% of the time
+    '''
+
+    # I'm thinking of..
+    # makig a hash table
+    # measure the occurances - histogram
+
+    # do if and else statements to choose
+
+    myList = ["erik","erik","erik","mel"]
+    table = {}
+
+    for i in myList:
+        table[i] = table.get(i, 0) + 1
+
+
+    return table
+
+
 if __name__ == "__main__":
-    test = sample()
-    print(test)
+    # test = sample()
+    random_index = random_index()
+    print(random_index)
