aa87782d08eb5df5d7ba3f74be0b79caa44ee00b
luxury <erikbatista42@gmail.com>
2019-02-01 18:48:54 -0800

    finished pg 3 histograms

--- histograms.py
+++ histograms.py
@@ -1,35 +1,35 @@
 def histogram(txt_file):
-    #return a histogram data structure that stores each unique word along with the number of times the word appears in the source text.
-    wordList = txt_file.read().split()
+    word_list = txt_file.read().split()
     table = dict()
 
-    for i in wordList:
-        table[i] = table.get(i, 0) + 1 # .get allows you to specify a default value if the key does not exist.
+    for i in word_list:
+        # puts the key as the word
+        # and puts the value as the number of times the key appears
+        table[i] = table.get(i, 0) + 1
+
     return table
 
 def unique_words(histogram):
-    #returns the total count of unique words in the histogram
     uniques = list()
+
     for key, value in histogram.items():
+        # if the value is 1, it means the word is unique
         if value == 1:
             uniques.append(key)
-    return len(uniques)
+        return len(uniques)
 
 def frequency(histogram, word):
-    #returns the number of times that word appears in a text.
-    #For example, when given the word "mystery" and the Holmes histogram, it will return the integer 20.
-
-    # goes into histogram table and gets value
+    # returns the num of times the word appears in a histogram
     return histogram[word]
 
 if __name__ == "__main__":
     file = open("20k.txt", "r")
 
-    histogram = histogram(file)
-    uniqueWordsInHistogram = unique_words(histogram)
-    frequency = frequency(histogram, "erik")
-
-    print("Histogram-> {} \nUnique words in histogram-> {} \nFrequency-> {}".format(histogram, uniqueWordsInHistogram, frequency))
+    file_histogram = histogram(file)
+    unique_words_in_histogram = unique_words(file_histogram)
+    frequency = frequency(file_histogram, "bio")
 
     file.close()
+
+    print("Unique words in histogram-> {} \nFrequency-> {}".format(unique_words_in_histogram, frequency))
 
