89f46f79531b7399481f86fafefd5b06e1d7bd78
luxury <erikbatista42@gmail.com>
2019-02-06 22:59:43 -0800

    finished and refactored historgrams.py

--- histograms.py
+++ histograms.py
@@ -1,61 +1,38 @@
-def table_histogram(txt_file):
-    word_list = txt_file.read().split()
+import random
+import sys
 
-
+def table_histogram(given_list):
     table = dict()
 
-    for i in word_list:
-        # puts the key as the word
-        # and puts the value as the number of times the key appears
+    for i in given_list:
+        # puts the KEY as the word
+        # and puts the VALUE as the number of times the key appears
         table[i] = table.get(i, 0) + 1
 
     return table
 
-def list_histogram(txt_file):
-    my_word_list = ["erik", "erik", "erik", "mel" ,"bob"]
-    new_list = []
 
-    # c = cute_array.split(" ")
-    # whole_list = list()
-    for w in my_word_list:
-        word_found = False
-        if len(new_list) == 0:
-            new_list.append([w,1])
+def list_histogram(word_list):
+    masterList = []
+    for wordIndex in range(len(word_list)):
+        modelList = [word_list[wordIndex], word_list.count(word_list[wordIndex])]
+        if modelList in masterList:
+            wordIndex += 1
         else:
-            for this_word in my_word_list:
-                if w in this_word[0]:
-                    word_found = True
-                    this_word[1] += 1
-                if not word_found:
-                    new_list.append([w,1])
-
-    # print(newList)
-    print(new_list)
+            masterList.append([word_list[wordIndex], word_list.count(word_list[wordIndex])])
+    return masterList
 
 
-def tuples_histogram(txt_file):
-    new_list = []
-    my_word_list = ["erik", "erik", "erik", "mel"]
+def tuples_histogram(word_list):
+    masterList = []
+    for wordIndex in range(len(word_list)):
+        modelTuple = (word_list[wordIndex], word_list.count(word_list[wordIndex]))
+        if modelTuple in masterList:
+            wordIndex += 1
+        else:
+            masterList.append((word_list[wordIndex], word_list.count(word_list[wordIndex])))
+    return masterList
 
-    # for w in my_word_list:
-    #     count = 0
-    #     for t in my_word_list:
-    #         if w == t:
-    #             count += 1
-    #     wtuple = (w, count)
-    #     if w not in new_list:
-    #         new_list.append(wtuple)
-    # print(new_list)
-
-    for i in my_word_list:
-        count = 1
-        for j in my_word_list:
-            if i == j:
-                my_word_list.remove(j)
-                count += 1
-        add = (i, count)
-        new_list.append(add)
-    print(new_list)
 
 def unique_words(histogram):
     uniques = list()
@@ -66,21 +43,30 @@ def unique_words(histogram):
             uniques.append(key)
         return len(uniques)
 
+
 def frequency(histogram, word):
-    # returns the num of times the word appears in a histogram
+    # returns the num of times the given word appears in a histogram
     return histogram[word]
 
+
 if __name__ == "__main__":
-    file = open("20k.txt", "r")
 
-    myWords = "one fish two fish red fish blue fish"
-    my_table_histogram = table_histogram(file)
+    fish_list = "one fish two fish red fish blue fish".split()
 
-    tuple_histogram = tuples_histogram(file)
+    # file = open("20k.txt", "r")
+    # word_list = file.read().split()
 
-    frequency = frequency(my_table_histogram, "bizjournalshire")
-    print(frequency)
-    file.close()
+    table_gram = table_histogram(fish_list)
+    print(table_gram)
 
-    # print("Unique words in histogram-> {} \nFrequency-> {}".format(unique_words_in_histogram, frequency))
+    list_gram = list_histogram(fish_list)
+    print(list_gram)
+
+    tuple_gram = tuples_histogram(fish_list)
+    print(tuple_gram)
+
+    frequency_of_word_in_table = frequency(table_gram, "fish")
+    print(frequency_of_word_in_table)
+
+    # file.close()
 
