203a24dca9af94f786fd914c90332a1b3a91c518
luxury <erikbatista42@gmail.com>
2019-02-07 00:04:12 -0800

    finished and refactored dictionary_words.py

--- dictonary_words.py
+++ dictonary_words.py
@@ -1,25 +1,29 @@
 import sys
 import random
 
-def getRandomWords():
-    #read the file
-    file = open("20k.txt", "r")
-
-    #turn all words in file into a list
-    wordList = file.read().split()
-
-    print(wordList)
-
-    file.close()
-
-    #take console argument num and get num of random words
-    numOfWordsWanted = int(sys.argv[1])
-    randWords = random.sample(wordList, numOfWordsWanted)
+def get_num_of_rand_words_from(given_list, num_of_words_wanted):
+    '''take console argument num and get num of random words'''
+    randWords = random.sample(given_list, num_of_words_wanted)
 
     #return a "sentence"
     return " ".join(randWords) + "."
 
 
 if __name__ == "__main__":
-    sentence = getRandomWords()
-    print(sentence)
+
+    fish_list = "one fish two fish red fish blue fish".split()
+
+    try:
+        '''num of words wanted typed in terminal argument'''
+        with open("20k.txt", "r") as file:
+            wordList = file.read().split()
+
+        wanted_words_num = int(sys.argv[1])
+
+        output_sentence = get_num_of_rand_words_from(wordList, wanted_words_num)
+        print(output_sentence)
+
+    except IndexError:
+        '''if no num input in terminal argument, use the fish list'''
+        fish_sentence = get_num_of_rand_words_from(fish_list, 2)
+        print(fish_sentence)
