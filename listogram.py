4afbd10afd18b87986a05b1e30c3e99aa700809c
luxury <erikbatista42@gmail.com>
2019-02-13 14:47:02 -0800

    listoogram index method completed

--- listogram.py
+++ listogram.py
@@ -34,6 +34,11 @@ class Listogram(list):
         """Return the index of entry containing given target word if found in
         this histogram, or None if target word is not found."""
         # TODO: Implement linear search to find index of entry with target word
+        for index, item in enumarate(self):
+            if target in item:
+                return index
+            else:
+                return None
 
 
 def print_histogram(word_list):
